4c8f738596740fb9b0da8efdb9f00fc4a602628a
thi-costa <thiago14abr@gmail.com>
2022-02-01 14:54:25 -0300

    Initial version

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 <p style="text-align:center; font-weight:bold;">
     Clone de aplicativo desenvolvido no curso de react native da digital innovation one
 </p>
-<p>Esse projeto eve como diferencial a criação de uma página de formulário para cadastro do perfil do cliente.</p>
+<p>As imagens abaixo ilustram o app.</p>
+<img src="">
--- src/Pages/Perfil/index.js
+++ src/Pages/Perfil/index.js
@@ -1,18 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
+import {useForm} from 'react-hook-form'
 import React from 'react';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Text } from 'react-native';
 import { SafeAreaView, Image, View, ButtonContainer } from './style';
 
+const Tab = createBottomTabNavigator();
 
 export default function Perfil() {
   return (
-    <>
-      <StatusBar style="theme-dark" />
-      <SafeAreaView>
-        <View>
-          <Text>Perfil</Text>
-        </View>
-      </SafeAreaView>
-    </>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Principal") {
+            iconName = "home";
+          } else if (route.name === "Perfil") {
+            iconName = "user";
+          }
+
+          
+          return <Feather name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "gray",
+      }}
+    >
+      <Tab.Screen name="Principal" component={Principal} />
+      <Tab.Screen name="Perfil" component={Perfil} />
+    </Tab.Navigator>
   );
 }
